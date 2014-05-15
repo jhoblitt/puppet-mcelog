e5e849ae10c7f37e5328cf377c33bb737df7bfad
Joshua Hoblitt <jhoblitt@cpan.org>
2014-05-15 11:17:17 -0700

    refactor init.pp to follow class autoloading rules
    * removed mcelog{install,config,service} classes all declared in the same file (init.pp)

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,32 +1,19 @@
 class mcelog {
-  include mcelog::install, mcelog::config, mcelog::service
-}
-
-class mcelog::install {
   package { 'mcelog':
     ensure => present,
-  }
-}
-
-class mcelog::config {
-
+  } ->
   file { '/etc/mcelog/mcelog.conf':
     ensure  => 'file',
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
     content => template('mcelog/mcelog.conf.erb'),
-    require => Class['mcelog::install'],
     notify  => Class['mcelog::service'],
-  }
-}
-
-class mcelog::service {
+  } ->
   service { 'mcelogd':
     ensure      => running,
     hasstatus   => true,
     hasrestart  => true,
     enable      => true,
-    require     => Class['mcelog::config'],
   }
 }
