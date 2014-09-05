070576caade226926cd7bb80ad06ee178eaa643a
Shawn Sterling <shawn@systemtemplar.org>
2014-09-05 01:15:27 -0700

    cleaning up lint errors + change a case to an if, moving vars into redhat, as we will fail if we aren't redhat anyway.

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,17 +1,16 @@
+# mcelog::params
 class mcelog::params {
-  $package_name         = 'mcelog'
-  $service_name         = 'mcelogd'
-  $config_file_template = 'mcelog/mcelog.conf.erb'
 
   # MCE is only supported on x86_64
-  case $::architecture {
-    'x86_64': {}
-    default: {
-      fail("Module ${module_name} is not supported on architecture: ${::architecture}")
-    }
+  if $::architecture != 'x86_64' {
+      fail("Module ${module_name} not supported on: ${::architecture}")
   }
+
   case $::osfamily {
     'redhat': {
+      $package_name         = 'mcelog'
+      $service_name         = 'mcelogd'
+      $config_file_template = 'mcelog/mcelog.conf.erb'
       case $::operatingsystemmajrelease {
         5: {
           $config_file_path = '/etc/mcelog.conf'
@@ -22,7 +21,8 @@ class mcelog::params {
           $service_manage   = true
         }
         default: {
-          fail("Module ${module_name} is not supported on operatingsystemmajrelease: ${::operatingsystemmajrelease}")
+          fail("Module ${module_name} is not supported on
+               operatingsystemmajrelease: ${::operatingsystemmajrelease}")
         }
       }
     }
