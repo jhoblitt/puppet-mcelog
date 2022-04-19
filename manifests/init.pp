1cb81b6f54b5320960e94b330d3460db0768f07c
Joshua Hoblitt <josh@hoblitt.com>
2022-04-19 14:04:34 -0700

    fix puppet-lint errors

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,17 +1,24 @@
-# mcelog class
+#
+# @summary Manage mcelog
+#
+# @param package_name
+#   The name of the package.
+# @param config_file_path
+#   The path of mcelog configuration file.
+# @param config_file_template
+#   The name of the [ERB] template to use for the generation of the `mcelog.conf` file.
+# @param service_name
+#   The name of the service.
+# @param service_stdout
+#   Only for EL7. The value of the StandardOutput parameter in the systemd script.
+#
 class mcelog (
-  $package_name         = $::mcelog::params::package_name,
-  $config_file_path     = $::mcelog::params::config_file_path,
-  $config_file_template = $::mcelog::params::config_file_template,
-  $service_name         = $::mcelog::params::service_name,
-  $service_stdout       = 'null',
+  String $package_name                   = $mcelog::params::package_name,
+  Stdlib::Absolutepath $config_file_path = $mcelog::params::config_file_path,
+  String $config_file_template           = $mcelog::params::config_file_template,
+  String $service_name                   = $mcelog::params::service_name,
+  String $service_stdout                 = 'null',
 ) inherits mcelog::params {
-
-  validate_string($package_name)
-  validate_string($config_file_path)
-  validate_string($config_file_template)
-  validate_string($service_name)
-
   package { $package_name:
     ensure => present,
   }
@@ -23,11 +30,11 @@ class mcelog (
     group   => 'root',
     mode    => '0644',
     content => template($config_file_template),
-    require => Package[$package_name]
+    require => Package[$package_name],
   }
 
   if $mcelog::params::service_manage {
-    if $::osfamily == 'Redhat' and $::operatingsystemmajrelease == '7' {
+    if $facts['os']['family'] == 'Redhat' and $facts['os']['release']['major'] == '7' {
       file { 'mcelog.service':
         ensure  => 'file',
         path    => '/usr/lib/systemd/system/mcelog.service',
@@ -36,12 +43,12 @@ class mcelog (
         mode    => '0644',
         content => template('mcelog/mcelog.service.erb'),
         notify  => Exec['systemd_reload'],
-        require => Package[$package_name]
+        require => Package[$package_name],
       }
       exec { 'systemd_reload':
         command     => '/usr/bin/systemctl daemon-reload',
         notify      => Service['mcelog'],
-        refreshonly => true
+        refreshonly => true,
       }
     }
     service { 'mcelog':
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,24 +1,23 @@
 # mcelog::params
 class mcelog::params {
-
   $package_name         = 'mcelog'
   $config_file_template = 'mcelog/mcelog.conf.erb'
 
   # MCE is only supported on x86_64
-  if ($::architecture != 'x86_64') and ($::architecture != 'amd64') {
-    fail("Module ${module_name} is not supported on architecture: ${::architecture}")
+  if ($facts['os']['architecture'] != 'x86_64') and ($facts['os']['architecture'] != 'amd64') {
+    fail("Module ${module_name} is not supported on architecture: ${facts['os']['architecture']}")
   }
 
-  case $::osfamily {
+  case $facts['os']['family'] {
     'RedHat': {
-      case $::operatingsystem {
+      case $facts['os']['name'] {
         'Fedora': {
           $config_file_path = '/etc/mcelog/mcelog.conf'
           $service_manage   = true
           $service_name     = 'mcelog'
         }
         default: {
-          case $::operatingsystemmajrelease {
+          case $facts['os']['release']['major'] {
             '5': {
               $config_file_path = '/etc/mcelog.conf'
               $service_manage   = false
@@ -35,26 +34,26 @@ class mcelog::params {
               $service_name     = 'mcelog'
             }
             default: {
-              fail("Module ${module_name} is not supported on operatingsystemmajrelease: ${::operatingsystemmajrelease}")
+              fail("Module ${module_name} is not supported on operatingsystemmajrelease: ${facts['os']['release']['major']}")
             }
           }
         }
       }
     }
     'Debian': {
-      case $::operatingsystemmajrelease {
+      case $facts['os']['release']['major'] {
         '6', '7', '8', '12.04', '14.04': {
           $config_file_path = '/etc/mcelog/mcelog.conf'
           $service_manage   = true
           $service_name     = 'mcelog'
         }
         default: {
-          fail("Module ${module_name} is not supported on operatingsystemmajrelease: ${::operatingsystemmajrelease}")
+          fail("Module ${module_name} is not supported on operatingsystemmajrelease: ${facts['os']['release']['major']}")
         }
       }
     }
     default: {
-      fail("Module ${module_name} is not supported on osfamily: ${::osfamily}")
+      fail("Module ${module_name} is not supported on osfamily: ${facts['os']['family']}")
     }
   }
 }
